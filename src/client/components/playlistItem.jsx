e32b23413aeef07ccf1a47b8bf3ffb7f5e0e94cb
Yi Xin Ang <angyixin@gmail.com>
2019-09-05 16:16:17 +0800

    Able to change the styling depending on which video i am watching

--- src/client/components/playlistItem.jsx
+++ src/client/components/playlistItem.jsx
@@ -8,8 +8,18 @@ class PlaylistItem extends React.Component{
   render() {
 
     console.log(this.props.song.name)
+
+    let playlistItemClasses = "";
+    if (this.props.song.status === "playing"){
+      playlistItemClasses = "playlist-item active"
+    }else if(this.props.song.status === "watched"){
+      playlistItemClasses = "playlist-item completed"
+    }else if(this.props.song.status === "unwatched"){
+      playlistItemClasses = "playlist-item"
+    }
+
      return (
-        <div className="playlist-item">
+        <div className={playlistItemClasses}>
             <div className="playlist-item-order">{this.props.song.order}</div>
             <div className="playlist-item-content">
                 <div className="song-info"><p>{this.props.song.name}</p> <p>{this.props.song.duration}</p></div>
