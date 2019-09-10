6d5ff0e62751c199323c50b5ee2940228ced56a1
Yi Xin Ang <angyixin@gmail.com>
2019-09-10 15:54:54 +0800

    nav component

---
+++ src/client/components/nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+class Nav extends React.Component {
+
+    render() {
+
+    return (
+      <div className="nav">
+        <h1 className="logo">Weraoke</h1>
+      </div>
+    );
+  }
+}
+
+export default Nav;
